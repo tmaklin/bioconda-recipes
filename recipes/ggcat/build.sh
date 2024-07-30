0841247b2ab51fc1f6e32b1b29f2b77c2df6fd54
Tommi Mäklin <tommi@maklin.fi>
2024-07-30 15:34:49 +0300

    Use find

--- recipes/ggcat/build.sh
+++ recipes/ggcat/build.sh
@@ -4,5 +4,7 @@ git submodule update --init --recursive
 
 cargo build --release --target-dir .
 
+path=$(find . -iname "ggcat")
+
 mkdir -p $PREFIX/bin
-cp release/ggcat $PREFIX/bin
+cp $path $PREFIX/bin
