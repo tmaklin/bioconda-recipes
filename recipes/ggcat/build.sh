9f7ccf44ebe555b8b2e84b7ef7188df4db5bd42b
Tommi Mäklin <tommi@maklin.fi>
2024-07-31 11:19:10 +0300

    fix6

--- recipes/ggcat/build.sh
+++ recipes/ggcat/build.sh
@@ -2,11 +2,11 @@
 
 git submodule update --init --recursive
 
-sed 's/ar cr/$(AR) cr/g' ggcat/crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
-mv tmp_makefile ggcat/crates/capi/ggcat-cpp-api/Makefile
+sed 's/ar cr/$(AR) cr/g' crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
+mv tmp_makefile crates/capi/ggcat-cpp-api/Makefile
 
-sed 's/g++/$(CXX)/g' ggcat/crates/capi/ggcat-cpp-api/example/Makefile > tmp_makefile
-mv tmp_makefile ggcat/crates/capi/ggcat-cpp-api/example/Makefile
+sed 's/g++/$(CXX)/g' crates/capi/ggcat-cpp-api/example/Makefile > tmp_makefile
+mv tmp_makefile crates/capi/ggcat-cpp-api/example/Makefile
 
 cargo build --release --target-dir .
 
