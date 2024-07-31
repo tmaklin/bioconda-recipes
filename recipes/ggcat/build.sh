f78bd0ad411bcda96b31ee118469a670fa8119b0
Tommi Mäklin <tommi@maklin.fi>
2024-07-31 15:12:28 +0300

    fix17

--- recipes/ggcat/build.sh
+++ recipes/ggcat/build.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-
-unamestr=`uname`
-
-if [ "$unamestr" == 'Darwin' ];
-then
-  export MACOSX_DEPLOYMENT_TARGET=10.15
-  export CFLAGS="${CFLAGS} -fcommon -D_LIBCPP_DISABLE_AVAILABILITY"
-  export CXXFLAGS="${CXXFLAGS} -fcommon -D_LIBCPP_DISABLE_AVAILABILITY"
-else
-  export CFLAGS="${CFLAGS} -fcommon"
-  export CXXFLAGS="${CXXFLAGS} -fcommon"
-  export C_INCLUDE_PATH="${C_INCLUDE_PATH}:${PREFIX}/include"
-  export CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH}:${PREFIX}/include"
-fi
-
 git submodule update --init --recursive
 
 sed 's/ar cr/$(AR) cr/g' crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
