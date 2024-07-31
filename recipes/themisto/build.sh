a1237f69a4a80b07de1412fd3221971037975afb
Tommi Mäklin <tommi@maklin.fi>
2024-07-31 17:33:11 +0300

    fix21

--- recipes/themisto/build.sh
+++ recipes/themisto/build.sh
@@ -23,11 +23,6 @@ echo '#include <cstdint>' | cat - ggcat/crates/capi/ggcat-cpp-api/src/ggcat.hh >
 
 sed 's/ar cr/$(AR) cr/g' ggcat/crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
 mv tmp_makefile ggcat/crates/capi/ggcat-cpp-api/Makefile
-sed 's/cargo build --release/cargo build --release --target-dir ./g' ggcat/crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
-mv tmp_makefile ggcat/crates/capi/ggcat-cpp-api/Makefile
-sed 's/cp[[:space:]].*$/LIBGGCAT_CPP_BINDINGS_PATH=$(shell find \/ -iname "libggcat_cpp_bindings.a")/g' ggcat/crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
-echo -e '\tcp $(LIBGGCAT_CPP_BINDINGS_PATH) ./lib/' >> tmp_makefile
-mv tmp_makefile ggcat/crates/capi/ggcat-cpp-api/Makefile
 
 sed 's/g++/$(CXX)/g' ggcat/crates/capi/ggcat-cpp-api/example/Makefile > tmp_makefile
 mv tmp_makefile ggcat/crates/capi/ggcat-cpp-api/example/Makefile
