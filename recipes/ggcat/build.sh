4550a445fd4e9b6b9e3cefec3913b39a839ed496
Tommi Mäklin <tommi@maklin.fi>
2024-07-31 12:56:56 +0300

    fix9

--- recipes/ggcat/build.sh
+++ recipes/ggcat/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+## Building rust packages requires MacOS SDK v > 10.12
+export MACOS_DEPLOYMENT_TARGET=12.7
+export MACOSX_DEPLOYMENT_TARGET=12.7
+
 git submodule update --init --recursive
 
 sed 's/ar cr/$(AR) cr/g' crates/capi/ggcat-cpp-api/Makefile > tmp_makefile
